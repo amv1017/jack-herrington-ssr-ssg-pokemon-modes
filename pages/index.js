e8a09c8ed6c9e19c249c520276fa6552f2d5cc3e
amv1017 <Vinokurov@bbs-c.ru>
2022-04-04 00:01:37 +0300

    SSG

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,47 @@
+// Static Site Generation
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export async function getStaticProps() {
+  const response = await fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json')
+  return {
+    props: {
+      pokemon: await response.json()
+    }
+  }
+}
+
+export default function Home({ pokemon }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pokemon List</title>
+      </Head>
+      <h2>Pokemon List</h2>
+      <div className={styles.grid}>
+        {pokemon.map((pokemon) => (
+          <div className={styles.card} key={pokemon.id}>
+            <Link href={`/pokemon/${pokemon.id}`}>
+              <a>
+                <img
+                  src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+                  alt={pokemon.name}
+                />
+                <h3>{pokemon.name}</h3>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+// Server Side Rendering
+/*
+
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -38,9 +82,10 @@ export default function Home({ pokemon }) {
   )
 }
 
-/*
+*/
 
 // Client Side Rendering
+/*
 
 import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
